3890f0feaa3b6c5ba6edaccfb660efb1de73259f
Vidut4000 <110621925+Vidut4000@users.noreply.github.com>
2022-09-20 22:51:07 +0530

    Add files via upload
    please make changes where  I didn't do better

---
+++ java1.js
@@ -0,0 +1,34 @@
+// text paragraph 
+document.getElementById("p1").innerHTML="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet omnis exercitationem nihil itaque recusandae cupiditate sit dolorem voluptas iusto maxime amet, doloribus vel veniam excepturi, doloremque quisquam debitis, ex iure laboriosam. Labore repellendus dignissimos ea ducimus repudiandae sed quaerat, cumque aut culpa laboriosam, possimus, pariatur est error ullam nisi molestiae quibusdam iure. Molestias voluptas placeat possimus nostrum obcaecati. Ipsa quam animi maiores temporibus aperiam expedita error tenetur adipisci modi vero exercitationem, libero doloribus porro nesciunt repellendus sapiente aliquam? Suscipit molestias magni pariatur tenetur unde dolorum cupiditate non quaerat nostrum sit consectetur animi voluptatem maiores vel, recusandae consequuntur fugit facilis aliquam."
+
+
+//document.get element by id method for some styling and writing text in html elements
+
+document.getElementById("h1").innerHTML="How add Css in HTML Document"
+document.getElementById("h2").innerHTML="3 ways to add Css in your html document";
+document.getElementById("h2").style.margin="30px";
+
+
+
+document.getElementById("p1").style.color="black";
+document.getElementById("p1").style.padding="10px";
+document.getElementById("p1").style.margin="30px";
+document.getElementById("p1").style.border="2px solid black";
+document.getElementById("p1").style.borderRadius="10px";
+document.getElementById("div_list").style.marginTop="20px";
+document.getElementById("div_list").style.marginLeft="50px";
+
+
+document.getElementById("1st_item").innerHTML="Inline Css";
+document.getElementById("2nd_item").innerHTML="Internal Css";
+document.getElementById("3rd_item").innerHTML="External Css";
+
+
+
+
+document.getElementById("Home").innerHTML="Home";
+document.getElementById("Services").innerHTML="Services";
+document.getElementById("Products").innerHTML="Products";
+document.getElementById("About Us").innerHTML="About Us";
+document.getElementById("Contact Us").innerHTML="Contact Us";
+
